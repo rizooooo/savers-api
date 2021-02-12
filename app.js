7251e2092b53df3951f0acf77097e0191bc6d0b1
DARRELL <drizojose0396@gmail.com>
2021-02-12 10:40:26 +0800

    Added port

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 
 const express = require('express')
 const app = express()
-const port = 3000
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.listen(process.env.PORT || port, () => {
+const port = process.env.PORT || 3000
+app.listen(port, () => {
     console.log(`Example app listening at ${port}`)
 })
