3245681eedf73b23a2fea8326960927124ce5c07
DARRELL <drizojose0396@gmail.com>
2021-02-12 11:00:27 +0800

    Added sequelize

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 
-const express = require('express')
-const app = express()
+const express = require('express');
+const DB = require('./config/db');
+const app = express();
+
+const authenticateDB = async () => {
+    try {
+        await DB.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+        process.exit(1);
+    }
+}
+
+const init = async () => {
+    await authenticateDB();
+
+    app.get('/', (req, res) => {
+        res.send('Hello World!')
+    });
+
+    const port = process.env.PORT || 3000
+
+    app.listen(port, () => {
+        console.log(`Savers API listening at PORT: ${port}`)
+    })
+}
 
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+init();
 
-const port = process.env.PORT || 3000
-app.listen(port, () => {
-    console.log(`Example app listening at ${port}`)
-})
